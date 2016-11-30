1493b18ad6c0c229a18457d7a505a0a6aec025f1
graholm <graholm@nuevaschool.org>
2016-11-30 09:00:40 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// hints!
 // 1. find the element in index.html that you want to copy
 // 2. select it somehow (by class . or id #)
 // 3. google 'jquery copy element'
@@ -6,10 +5,12 @@
 // 5. how do you repeat this multiple times, for multiple colors??? There is a nice data structure for this kind of thing.
 
 var elementsList = $("#elements-list")
-
 var el= $( ".element" )
 $(elementsList).append(el.clone())
 $(elementsList).append(el.clone())
+$(elementsList).append(el.clone())
+$(elementsList).append(el.clone())
+$(elementsList).append(el.clone())
                       
-
+var a =
 
